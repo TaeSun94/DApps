7eaea4d5316bb4370844f87411e68d408128b583
Taesun 94 <tyzlddy@naver.com>
2022-01-10 19:10:51 +0900

    feat: Chat Page init

--- pages/chat/index.js
+++ pages/chat/index.js
@@ -1,7 +1,30 @@
 
-export default function(userAddress){
-    console.log(userAddress);
+import React from 'react';
+import Modal from '../../components/Modal';
+import Styled from './chatStyle';
+
+
+export default function(){
+    const [isLoad, setIsLoad] = React.useState(false);
+    const [showModal, setShowModal] = React.useState(false);
+    //TO-DO
+    //page를 Link로 이동할 때 props내에 account를 보내줄 수 있다면 첫 실행 단계에서의 마운트작업이 필요
+    //React.useEffect(()=>{});
+    React.useEffect(()=>{
+        console.log("set")
+    },[isLoad]);
     return(
-        <></>
+        <div>
+        {
+            !isLoad ?
+            <>
+                <button onClick={()=> setShowModal(true)}>Open Modal</button>;
+                <Modal onClose={()=> setShowModal(false)} show={showModal}> Connect M</Modal>
+            </>
+            :
+            <>
+            </>
+        }
+        </div>
     )
 }
