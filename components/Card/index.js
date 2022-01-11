0e2ff196b584976426285cbe97b1ede0ae08886d
Taesun 94 <tyzlddy@naver.com>
2022-01-11 19:08:06 +0900

    [+] Mod Link로 page를 이동할 때 query를 통해서 props를 보내도록 변경

--- components/Card/index.js
+++ components/Card/index.js
@@ -1,9 +1,14 @@
 import Link from 'next/link';
 import Styled from './cardStyle';
 const Card = ({...props}) => {
-
+    
     return (
-        <Link href={props.link}>
+        <Link 
+            href={{
+                pathname: `${props.link}`,
+                query: {address: `${props.address}`}
+            }}
+        >
             <Styled.Container>
                 <h2>{props.title} &rarr;</h2>
                 <p>{props.description}</p>
