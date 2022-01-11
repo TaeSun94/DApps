cfaf86d6da9ef3b5e7160a39f4dfd1b3e0755439
Taesun 94 <tyzlddy@naver.com>
2022-01-11 19:08:56 +0900

    [+] Add Chating Page 1차 구현

--- pages/chat/chatStyle.js
+++ pages/chat/chatStyle.js
@@ -1,7 +1,47 @@
 
-import Styled from 'styled-components';
+import styled from 'styled-components';
 
-const Container = Styled.div`
-
+const Container = styled.div`
+    margin: 60px;
+    flex-direction: 'colunm';
 `;
-export default {Container};
+
+const Main = styled.div`
+    min-height: 100vh;
+    padding: 4rem 0;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const Head = styled.div`
+    
+`;
+
+const Log = styled.div`
+    display: block;
+    width: 100%;
+    overflow: auto;
+    height: 600px;
+    border: solid;
+    padding: 5px;
+    border-radius: '15%';
+`;
+
+const Input = styled.div`
+    margin-top: 20px;
+    width: 100%;
+    min-height: 30px;
+    border: solid;
+    flex-direction: row;
+    alignItems:'center';
+    justifyContent:'center';
+    .inputBox {
+        background-color: red;
+        width:90%;
+        height:100%;
+    }
+    .submitBtn{
+        backgroundColor: white;
+    }
+`;
+export default {Container, Main, Log, Input, Head};
