59a951803dc8e7e9cd97c853f80a4d4f314d3c38
Taesun 94 <tyzlddy@naver.com>
2022-01-12 18:33:59 +0900

    [+] Add 입퇴장 및 매세지에 따른 다른 ui 적용

---
+++ components/Messages/index.js
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import Message from './Message';
+const Messages = ({...props}) => {
+    return(
+        <div>
+        {
+            props.messages.map((msg, idx)=> (
+                <div key={idx}>
+                    {
+                        msg.type === 0 ?
+                        <div style={{textAlign:'center',marginTop:'5px',marginBottom:'5px'}}>
+                            <text>{msg.message}</text>
+                        </div>
+                        :
+                        <Message message={msg.message} />
+                    }
+                </div>
+            ))
+        }
+        </div>
+    )
+}
+
+export default Messages;
