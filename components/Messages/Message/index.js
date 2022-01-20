fe81d35b5f68ea3a6c381b24b1b100c0b52c1222
Taesun 94 <tyzlddy@naver.com>
2022-01-20 19:13:08 +0900

    Fix: 우클릭시 context Menu 나타나도록 설정

--- components/Messages/Message/index.js
+++ components/Messages/Message/index.js
@@ -1,44 +1,53 @@
 
 import Styled from './messageStyle';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu";
 import {FaRegCopy, FaList,FaEllipsisV, FaShareAlt} from 'react-icons/fa'
 import {RiSendPlaneFill, RiDeleteBin6Line} from 'react-icons/ri'
+import { contextMenu, Item, Menu, useContextMenu } from 'react-contexify';
+import "react-contexify/dist/ReactContexify.min.css";
+const menuId = "RightClickMenu";
 
 const Message = ({messageItem,own, mint}) => {
-    
+    // const {show} = useContextMenu({id: menuId});
+
+    const handleRight = e => {
+        e.preventDefault();
+        contextMenu.show({
+            id: menuId,
+            event: e,
+        })
+        // mint(messageItem);
+    }
+
+    const handlerCheck = () =>{
+        mint(messageItem);
+    } 
+
     return (
         <Styled.Div>
             <div style={{marginLeft:'24px',paddingBottom:'0px'}}>
                 <text>User: {messageItem.user}</text>
             </div>
-            <ContextMenuTrigger id="contextmenu">
-                {
-                    own ?
-                    <Styled.OwnContainer>
-                        <div style={{display:'flex', justifyContent:'center', flexDirection:'row', }}>
-                        <p>{messageItem.message}</p>
-                        </div>
-                    </Styled.OwnContainer>
-                    :
-                    <Styled.Container>
-                        <div style={{display:'flex', justifyContent:'center', flexDirection:'row'}}>
-                        <p>{messageItem.message}</p>
-                        </div>
-                    </Styled.Container>
-                }
-            </ContextMenuTrigger>
-            <ContextMenu id="contextmenu">
-                <MenuItem onClick={
-                    ()=>{
-                        mint(messageItem);
-                        // console.log(messageItem);
-                    }}
-                >
-                    <RiSendPlaneFill className="send"/>
-                    <span>Minting</span>
-                </MenuItem>
-            </ContextMenu>
+            {
+                own ?
+                <Styled.OwnContainer onContextMenu={handleRight}>
+                    <div style={{display:'flex', justifyContent:'center', flexDirection:'row', }}>
+                    <p>{messageItem.message}</p>
+                    </div>
+                </Styled.OwnContainer>
+                :
+                <Styled.Container onContextMenu={handleRight}>
+                    <div style={{display:'flex', justifyContent:'center', flexDirection:'row'}}>
+                    <p>{messageItem.message}</p>
+                    </div>
+                </Styled.Container>
+            }
+            <Menu id={menuId}>
+                <Item onClick={handlerCheck}>
+                    mint
+                </Item>
+            </Menu>
         </Styled.Div>
     )
 }
